f7f5cdbc1768dc39cefcef18feedc7fd2efbf1a8
adrivr99 <adri.vallejo.99@gmail.com>
2021-06-01 21:30:05 +0200

    Listas 1 y 2

--- src/main/java/dominolista/Test.java
+++ src/main/java/dominolista/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dominolista;
+package main.java.dominolista;
 
 import java.util.Scanner;
 
